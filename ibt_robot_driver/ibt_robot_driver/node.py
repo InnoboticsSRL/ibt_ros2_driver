5dbbbf9401b49813307751d2a4d397f4850d765f
Mattia Dei Rossi <deirossi@automationware.it>
2024-11-06 14:55:00 +0100

    refactor, renamed variables

--- ibt_robot_driver/ibt_robot_driver/node.py
+++ ibt_robot_driver/ibt_robot_driver/node.py
@@ -12,6 +12,7 @@ from std_srvs.srv import Empty
 from sensor_msgs.msg import JointState
 from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from control_msgs.action import FollowJointTrajectory
+from rclpy.callback_groups import ReentrantCallbackGroup, MutuallyExclusiveCallbackGroup
 
 from gbp.ros import with_asyncio, AsyncIoSupport, Ros2LoggingHandler, Ros2Spinner
 from gbp.connection import GbcClient
@@ -35,13 +36,13 @@ class RobotDriver(LifecycleNode, AsyncIoSupport):
         self.declare_parameter('timer_period', value=0.05)
 
         # get parameters
-        self.url = self.get_parameter('url').value
-        self.use_fake = self.get_parameter('use_fake').value
-        self.timer_period = self.get_parameter('timer_period').value
+        self._url = self.get_parameter('url').value
+        self._use_fake = self.get_parameter('use_fake').value
+        # self.timer_period = self.get_parameter('timer_period').value
 
         # gbc connection
-        self.gbc = GbcClient(self.url)
-        self.gbc.register(
+        self._gbc = GbcClient(self._url)
+        self._gbc.register(
             Ros2Spinner(self),
             HeatbeatEcho(),
             OperationErrorLogger(),
@@ -53,7 +54,7 @@ class RobotDriver(LifecycleNode, AsyncIoSupport):
             JointState, 'joint_states', 10)
         
         # services
-        self.enable = self.create_service(Empty, 'enable', self.enable_callback)
+        self._enable = self.create_service(Empty, 'enable', self.enable_callback)
         
         # moveit action server
         self._action_server = ActionServer(
@@ -61,14 +62,16 @@ class RobotDriver(LifecycleNode, AsyncIoSupport):
             FollowJointTrajectory,
             'follow_joint_trajectory',
             execute_callback=self.execute_callback,
+            callback_group=ReentrantCallbackGroup(),
             goal_callback=self.goal_callback,
             cancel_callback=self.cancel_callback,
             handle_accepted_callback=self.handle_accepted_callback)
 
-        self.goal = None
+        self._goal = None
+        self._goal_handle = None
 
     async def connect(self):
-        await self.gbc.connect(blocking=True)
+        await self._gbc.connect(blocking=True)
     
     def destroy(self):
         self.get_logger().info("Destroying node")
@@ -81,28 +84,29 @@ class RobotDriver(LifecycleNode, AsyncIoSupport):
     @with_asyncio(timeout=60)
     async def enable_callback(self, request, response):
         self.get_logger().info('Robot enabled')
-        await self.gbc.run_once(OpEnabledEffect(), lambda op: op.enable_operation())
+        await self._gbc.run_once(OpEnabledEffect(), lambda op: op.enable_operation())
         return response
     
     def goal_callback(self, goal_):
         self.get_logger().info('Goal request recieved')
-        self.goal = goal_
+        self._goal = goal_
         return GoalResponse.ACCEPT
 
     def cancel_callback(self, goal_handle):
         # Accepts or rejects a client request to cancel an action
         self.get_logger().info("Stopping initiated")
         self._stop_move(goal_handle)
-        goal_handle.canceled()
+        goal_handle = None
         return CancelResponse.ACCEPT
 
     def handle_accepted_callback(self, goal_handle):
         # Takes care of multiple goal requests
-        if goal_handle.is_active:
+        if self._goal_handle and self._goal_handle.is_active:
             self.get_logger().info("Stopping the executing goal")
-            self._stop_move()
-            goal_handle.canceled()
+            self._stop_move(goal_handle)
+            self._goal_handle.canceled()
 
+        self._goal_handle = goal_handle
         self.get_logger().info("Executing the new goal")
         goal_handle.execute()
 
@@ -110,37 +114,36 @@ class RobotDriver(LifecycleNode, AsyncIoSupport):
     async def execute_callback(self, goal_handle):
         self.get_logger().info(f"Executing goal")
         try:
-            points = self.goal.trajectory.points
+            points = self._goal.trajectory.points
             feedback = FollowJointTrajectory.Feedback()
             feedback.header = Header()
             feedback.header.frame_id = 'awtube_base_link' # TODO parametric
             result = FollowJointTrajectory.Result()
 
-            async def stream_callback(stream: Stream):
-                await stream.exec(
-                    ActivityStreamItem(
-                        activityType=ACTIVITYTYPE.ACTIVITYTYPE_MOVEJOINTSINTERPOLATED, moveJointsInterpolated=MoveJointsInterpolatedStream(
-                            jointPositionArray=points.positions,
-                            jointVelocityArray=points.velocities,
-                        )
-                    )
-                )
+            # async def stream_callback(stream: Stream):
+            #     await stream.exec(
+            #         ActivityStreamItem(
+            #             activityType=ACTIVITYTYPE.ACTIVITYTYPE_MOVEJOINTSINTERPOLATED, moveJointsInterpolated=MoveJointsInterpolatedStream(
+            #                 jointPositionArray=points.positions,
+            #                 jointVelocityArray=points.velocities,
+            #             )
+            #         )
+            #     )
 
-            await self.gbc.run_once(Stream(0), stream_callback)
-
+            # await self._gbc.run_once(Stream(0), stream_callback)
+            goal_handle.succeed()
+            self._goal_handle = None
+            result.error_code = result.SUCCESSFUL
+            result.error_string = "Completed"
+            self.get_logger().info("FollowJointTrajectory Completed the Goal")
+            return result
+        
         except Exception:
             self._stop_move(goal_handle)
             goal_handle.canceled()
-            self.get_logger().info("Stop server.")
-            result.error_code = result.INVALID_GOAL
+            # result.error_code = result.INVALID_GOAL
             result.error_string = "Cancelled"
             return result
-
-        goal_handle.succeed()
-        result.error_code = result.SUCCESSFUL
-        result.error_string = "Completed"
-        self.get_logger().info("FollowJointTrajectory Completed the Goal")
-        return result
 
     def _stop_move(self, goal_handle):
         # TODO implement a proper logic
