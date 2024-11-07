70bf9d7304ccca11a33633b7188fc9bd3a13582a
Mattia Dei Rossi <deirossi@automationware.it>
2024-11-07 14:45:35 +0100

    signal handler added

--- ibt_robot_driver/ibt_robot_driver/main.py
+++ ibt_robot_driver/ibt_robot_driver/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
 import rclpy
+import signal
 
 from ibt_robot_driver.node import RobotDriver
 
@@ -8,4 +9,12 @@ async def main():
     rclpy.init()
     await RobotDriver(asyncio.get_running_loop()).connect()
 
-asyncio.run(main())
+def handle_sigterm(*args):
+    print("Received SIGTERM, shutting down...")
+    raise KeyboardInterrupt
+
+try:
+    signal.signal(signal.SIGTERM, handle_sigterm)
+    asyncio.run(main())
+except KeyboardInterrupt:
+    print("KeyboardInterrupt")
